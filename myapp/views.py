11bfc8fd3705d37755391963bb38a35a74c3cd01
taes-k <cobura12@gmail.com>
2019-03-29 20:39:15 +0900

    template update

--- myapp/views.py
+++ myapp/views.py
@@ -3,4 +3,5 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse ("Hello, World!")
+    return render(request, "index.html")
+    # return HttpResponse ("Hello, World!")
